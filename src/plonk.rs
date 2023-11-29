5369aef6051d6d7a5e0a7f540fe1ff8610fed94f
David Wong <davidwong.crypto@gmail.com>
2023-11-29 10:53:09 -0800

    added plonk vk

---
+++ src/plonk.rs
@@ -0,0 +1,19 @@
+/// The snarkjs plonk verifier key format.
+pub struct VerifierKey {
+    protocol: String, // "plonk",
+    curve: String,    // "bn128",
+    nPublic: usize,   // 96,
+    power: usize,     // 9,
+    k1: String,       // "2",
+    k2: String,       // "3",
+    Qm: Vec<String>,
+    Ql: Vec<String>,
+    Qr: Vec<String>,
+    Qo: Vec<String>,
+    Qc: Vec<String>,
+    S1: Vec<String>,
+    S2: Vec<String>,
+    S3: Vec<String>,
+    X_2: Vec<Vec<String>>,
+    w: String, //"6837567842312086091520287814181175430087169027974246751610506942214842701774"
+}
