2a6c1b1288edd6dd768c4f68bfe09b396e100230
David Wong <davidwong.crypto@gmail.com>
2023-11-28 21:38:44 -0800

    sighash template

--- src/mpc_sign_tx.rs
+++ src/mpc_sign_tx.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use bitcoin::{Amount, PublicKey, Transaction};
+use bitcoin::{sighash, Amount, PublicKey, Transaction};
 
 use crate::{
     bob_request::BobRequest,
@@ -102,4 +102,32 @@ pub async fn validate_request(request: BobRequest) -> Result<(), &'static str> {
 
     // TODO: ensure that the hash of the VK correctly gives us the vk_hash
     Ok(())
+}
+
+pub fn sign_transaction_ecdsa(
+    sk: &secp256k1::SecretKey,
+    tx: &Transaction,
+) -> secp256k1::ecdsa::Signature {
+    // TODO: figure out how to get the digest to sign
+    // the C++ code: https://github.com/bitcoin/bitcoin/blob/16b5b4b674414c41f34b0d37e15a16521fb08013/src/script/sign.cpp#L784
+    let msg = {
+        let mut cache = sighash::SighashCache::new(tx);
+        let sighash = cache
+            .p2wpkh_signature_hash(inp_idx, &spk, Amount::from_sat(value), sig.hash_ty)
+            .expect("failed to compute sighash");
+
+        println!("Segwit p2wpkh sighash: {:x}", sighash);
+
+        secp256k1::Message::from_digest(sighash.to_byte_array())
+    };
+
+    let secp = secp256k1::Secp256k1::signing_only();
+    let sig = secp.sign_ecdsa(&msg, &sk);
+
+    let pubkey = sk.public_key(&secp);
+
+    let secp = secp256k1::Secp256k1::verification_only();
+    secp.verify_ecdsa(&msg, &sig, &pubkey).unwrap();
+
+    sig.sig
 }
