1fc6bd15d32119cd988bd3762444a9a3038c1035
David Wong <davidwong.crypto@gmail.com>
2023-12-01 11:54:36 -0800

    cli: clean up

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -6,19 +6,18 @@ use zkbitcoin::{alice_sign_tx::generate_and_broadcast_transaction, json_rpc_stuf
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
+    // /// Optional name to operate on
+    // name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
+    // /// Sets a custom config file
+    // #[arg(short, long, value_name = "FILE")]
+    // config: Option<PathBuf>,
 
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
+    // /// Turn debugging information on
+    // #[arg(short, long, action = clap::ArgAction::Count)]
+    // debug: u8,
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
@@ -57,34 +56,34 @@ async fn main() {
     let cli = Cli::parse();
 
     // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
+    // if let Some(name) = cli.name.as_deref() {
+    //     println!("Value for name: {name}");
+    // }
 
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
+    // if let Some(config_path) = cli.config.as_deref() {
+    //     println!("Value for config: {}", config_path.display());
+    // }
 
     // You can see how many times a particular flag or argument occurred
     // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
+    // match cli.debug {
+    //     0 => println!("Debug mode is off"),
+    //     1 => println!("Debug mode is kind of on"),
+    //     2 => println!("Debug mode is on"),
+    //     _ => println!("Don't be crazy"),
+    // }
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Some(Commands::DeployTransaction {
+        Commands::DeployTransaction {
             wallet,
             verifier_key_path,
             public_inputs_path,
             address,
             auth,
             satoshi_amount,
-        }) => {
+        } => {
             let ctx = RpcCtx::new(wallet.clone(), address.clone(), auth.clone());
 
             let (vk, vk_hash) = {
@@ -145,6 +144,5 @@ async fn main() {
                     .await
                     .unwrap();
         }
-        None => {}
     }
 }
