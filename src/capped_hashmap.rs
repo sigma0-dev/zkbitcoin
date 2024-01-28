f061aaddf3de716916dbb7be09f3129ec0450d18
ppoliani <ppoliani@gmail.com>
2024-01-28 12:32:00 +0200

    test: add test_insert_should_return_removed_key

--- src/capped_hashmap.rs
+++ src/capped_hashmap.rs
@@ -28,19 +28,20 @@ where
     /// Inserts an new item to the collection. Return Some(key) where key is the
     /// key that was removed when we reach the max capacity. Otherwise returns None.
     pub fn insert(&mut self, k: K, v: V) -> Option<K> {
-        self.last_tasks.push_front(k);
+        let mut ret = None;
 
         if self.last_tasks.len() == self.capacity {
             // remove the oldest item. We an safely unwrap because we know the last_tasks is not empty at this point
             let key = self.last_tasks.pop_back().unwrap();
-            self.remove(&key);
+            assert!(self.remove(&key).is_some());
 
-            return Some(key);
+            ret = Some(key);
         }
 
+        self.last_tasks.push_front(k);
         self.inner.insert(k, v);
 
-        None
+        ret
     }
 
     /// Removes a key from the map, returning the value at the key if the key was previously in the map.
@@ -61,14 +62,14 @@ where
 
     /// Returns a reference to the value corresponding to the key.
     pub fn get(&self, k: &K) -> Option<&V> {
-      self.inner.get(k)
+        self.inner.get(k)
     }
 
     /// Returns a mutable reference to the value corresponding to the key.
     pub fn get_mut(&mut self, k: &K) -> Option<&mut V> {
-      self.inner.get_mut(k)
+        self.inner.get_mut(k)
     }
-    
+
     /// Returns the number of elements in the collection
     pub fn size(&self) -> usize {
         self.inner.len()
@@ -81,13 +82,30 @@ mod tests {
 
     #[test]
     fn test_insert_into_non_full_collection() {
-        let mut col: CappedHashMap<&str, u8> = CappedHashMap::new(10);
-        col.insert("key_1", 1);
-        col.insert("key_2", 2);
-        col.insert("key_3", 3);
+        let mut col: CappedHashMap<u8, u8> = CappedHashMap::new(10);
+        col.insert(1, 1);
+        col.insert(2, 2);
+        col.insert(3, 3);
 
-        assert_eq!(*col.get(&"key_1").unwrap(), 1);
-        assert_eq!(*col.get(&"key_2").unwrap(), 2);
-        assert_eq!(*col.get(&"key_3").unwrap(), 3);
+        assert_eq!(*col.get(&1).unwrap(), 1);
+        assert_eq!(*col.get(&2).unwrap(), 2);
+        assert_eq!(*col.get(&3).unwrap(), 3);
+    }
+
+    #[test]
+    fn test_insert_should_return_removed_key() {
+        let mut col: CappedHashMap<u8, u8> = CappedHashMap::new(10);
+
+        for i in 0..10 {
+            col.insert(i, i);
+        }
+
+        for i in 10..30 {
+          // the nth oldest key will be removed
+          let key_removed = col.insert(i, i);
+          assert!(key_removed.is_some());
+          assert_eq!(key_removed.unwrap(), i - 10);
+          assert_eq!(col.size(), 10);
+        }
     }
 }
