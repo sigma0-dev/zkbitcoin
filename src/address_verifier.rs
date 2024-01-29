0118e004ce746c5000cb5c9048e9d3d22a310ba2
ppoliani <ppoliani@gmail.com>
2024-01-29 18:45:43 +0200

    feat: extract publish_date

--- src/address_verifier.rs
+++ src/address_verifier.rs
@@ -1,15 +1,19 @@
-use log::error;
+use anyhow::{Context, Result};
+use chrono::prelude::*;
+use fancy_regex::Regex;
+use futures::StreamExt;
+use log::{error, info};
 use std::{
     collections::HashMap,
     sync::Arc,
-    time::{Duration, UNIX_EPOCH},
+    time::{Duration, Instant, UNIX_EPOCH},
 };
 use tokio::{spawn, sync::RwLock, time::interval};
 use xml::reader::{EventReader, XmlEvent};
 
 pub struct AddressVerifier {
     sanctioned_addresses: Arc<RwLock<HashMap<String, bool>>>,
-    last_update: Arc<RwLock<u64>>,
+    last_update: Arc<RwLock<i64>>,
 }
 
 impl AddressVerifier {
@@ -18,12 +22,43 @@ impl AddressVerifier {
     pub fn new() -> Self {
         Self {
             sanctioned_addresses: Arc::new(RwLock::new(HashMap::new())),
-            last_update: Arc::new(RwLock::new(Self::now())),
+            last_update: Arc::new(RwLock::new(0)),
         }
     }
 
-    fn now() -> u64 {
-        UNIX_EPOCH.elapsed().unwrap().as_secs()
+    fn extract_from_xml(str_value: &str, tag: &str) -> Result<u32> {
+        let re = Regex::new(&format!(r"(?<={}>)\s*(\w+)(?=<\/{})", tag, tag)).unwrap();
+        let value = re.find(&str_value)?.context("no regex result")?.as_str();
+
+        Ok(value.parse()?)
+    }
+
+    /// read the first few bytes from the remote XML file and extract the last update date.
+    /// If there is no fresh data we can skip the parsing of XML which is slow.
+    async fn publish_date() -> Result<i64> {
+        let res =
+            reqwest::get("https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml")
+                .await?;
+
+        let head = res
+            .bytes_stream()
+            .take(1)
+            .collect::<Vec<reqwest::Result<_>>>()
+            .await
+            .into_iter()
+            .collect::<reqwest::Result<Vec<_>>>()?;
+
+        let str_value = String::from_utf8(head[0].to_vec())?;
+        let year = Self::extract_from_xml(&str_value, "Year")?;
+        let day = Self::extract_from_xml(&str_value, "Day")?;
+        let month = Self::extract_from_xml(&str_value, "Month")?;
+        let date = Utc
+            .with_ymd_and_hms(year as i32, month, day, 0, 0, 0)
+            .single()
+            .context("date parse error")?
+            .timestamp();
+
+        Ok(date)
     }
 
     /// Periodically fetces the latest list from https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml
@@ -36,12 +71,22 @@ impl AddressVerifier {
             let mut interval = interval(Duration::from_secs(600));
 
             loop {
+                let Ok(publish_date) = Self::publish_date().await else {
+                    error!("couldn't extract the OFAC document publish date");
+                    continue;
+                };
+
+                if *last_update.read().await >= publish_date {
+                    continue;
+                }
+
                 let mut sanctioned_addresses = sanctioned_addresses.write().await;
                 interval.tick().await;
-                // TODO:: read the first few bytes from the remote XML file and extract the last update date.
-                // If there is no fresh data we can skip the parsing of XML which is slow.
-                let mut _last_update = last_update.write().await;
 
+                let mut last_update = last_update.write().await;
+                *last_update = publish_date;
+
+                let start = Instant::now();
                 let Ok(res) = reqwest::get(
                     "https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml",
                 )
@@ -91,6 +136,9 @@ impl AddressVerifier {
                         _ => {}
                     }
                 }
+
+                let duration = start.elapsed();
+                info!("OFAC list update exec time {:?}", duration);
             }
         })
         .await
