4bc5140ba33ecead02df9f52b38fea2fbeff21b6
ppoliani <ppoliani@gmail.com>
2024-02-04 16:08:25 +0200

    fix: linting issues

--- src/utils/version.rs
+++ src/utils/version.rs
@@ -4,7 +4,7 @@ use reqwest::Client;
 use serde::Deserialize;
 use versions::Versioning;
 
-const RELEASES_URL: &'static str =
+const RELEASES_URL: &str =
     "https://api.github.com/repos//sigma0-xyz/zkbitcoin/releases/latest";
 
 #[derive(Deserialize, Debug)]
@@ -30,7 +30,7 @@ async fn fetch_latest_version() -> Result<Release> {
 pub async fn check_version() -> Result<()> {
     let current_version = Versioning::new(env!("CARGO_PKG_VERSION"));
     let latest_release = fetch_latest_version().await?;
-    let latest_version = Versioning::new(&latest_release.tag_name.replace("v", ""));
+    let latest_version = Versioning::new(&latest_release.tag_name.replace('v', ""));
 
     if current_version < latest_version {
         warn!(
